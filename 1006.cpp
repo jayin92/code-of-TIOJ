55695c05750211cd28ebcb008bbda42e7af90786
jayinnn <jayin920805@gmail.com>
2019-02-09 11:13:25 +0800

    finish big number multiply

--- 1006.cpp
+++ 1006.cpp
@@ -24,6 +24,33 @@ string compare(string a, string b){
     return "1";
 }
 
+void normalize(vector<int> a){
+    int carry = 0;
+    for(int i=0;;i++){
+        carry = a[i] / 10;
+        a[i] %= 10;
+        if(i+1>=a.size() && carry != 0){
+            a.push_back(carry);
+        } else if(i+1>=a.size() && carry == 0){
+            break;
+        } else {
+            a[i+1] += carry;
+        }
+    }
+
+    bool flag = 0;
+    for(int i=a.size();i>=0;i--){
+        if(flag){
+                cout << a[i];
+        }
+        if(a[i] != 0 && flag == 0){
+            cout << a[i];
+            flag = 1;
+        }
+    }
+    cout << endl;    
+}
+
 void add(string a, string b){
     int MAX = max(a.size(), b.size())+1;
     
@@ -104,6 +131,7 @@ void minus1(string a, string b){
             rst[i] += (e[i] - f[i]);
         }
     }
+
     bool flag = 0;
     for(int i=MAX-1;i>=0;i--){
         if(flag){
@@ -115,7 +143,40 @@ void minus1(string a, string b){
         }       
     }
     cout << endl;
+}
 
+void mutiply(string a, string b){
+    int c[a.size()] = {0};
+    int d[b.size()] = {0};
+    vector<int> rst;
+
+    int j = 0;
+    for(int i=a.size()-1;i>-1;i--){
+        c[j] += (int)a[i] - (int)'0';
+        j++;
+    }
+    j = 0;
+    for(int i=b.size()-1;i>-1;i--){
+        d[j] += (int)b[i] - (int)'0';
+        j++;
+    }
+    int rst_len;
+    // int j at here just like a pointer, point the init digit
+    for(int i=0;i<b.size();i++){
+        rst_len = rst.size();
+        for(int j=0;j<a.size();j++){
+            if(rst_len<j+i+1){
+                rst.push_back(c[j]*d[i]);
+            } else {
+                rst[j+i] += c[j]*d[i];
+            }
+        }         
+    }
+    normalize(rst);
+}
+
+void subtract(string a, string b){
+    
 }
 
 
@@ -127,6 +188,5 @@ int main(){
 
     add(a, b);
     minus1(a, b);
-
-
+    mutiply(a, b);
 }
